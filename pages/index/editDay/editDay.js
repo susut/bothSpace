cbc9892186c351da089f9dd2222ec4c07011bc11
chensuting <chensuting@ruqimobility.com>
2021-05-15 10:27:49 +0800

    feat: some idea

---
+++ pages/index/editDay/editDay.js
@@ -0,0 +1,6 @@
+// pages/index/editDay/editDay.js
+Page({
+  data: {
+    remindTimeList: ['不提醒', '提前一天', '提前三天', '提前七天']
+  }
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,50 +1,14 @@
 //index.js
-//获取应用实例
-const app = getApp();
-import { itemMap, categoryList} from '../../utils/data';
 
 Page({
   data: {
-    motto: 'aaa',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    categoryList,
-    curCategoryIndex: 0, // 默认选中第一个
-    curItemList: itemMap[categoryList[0].key]
+    
   },
-  onLoad: function () {
+  onLoad() {
   },
-  changeCategory: function (e) {
-    const { index = 0 } = e.currentTarget.dataset;
-    const itemItemList = itemMap[categoryList[index].key];
-    this.setData({
-      curCategoryIndex: index,
-      curItemList: itemItemList
+  editDay() {
+    wx.navigateTo({
+      url: '/pages/index/editDay/editDay'
     });
-  },
-  addToCart: function (e) {
-    const { item = {} } = e.currentTarget.dataset;
-    const { cartList = [] } = app.globalData;
-    let has = false;
-    cartList.forEach(li => {
-      if (!has && item.id === li.id) {
-        li.buyNum++;
-        has = true;
-      }
-    });
-    if (!has) {
-      let copyItem = { ...item };
-      copyItem.buyNum = 1;
-      cartList.push(copyItem);
-    }
-    app.globalData.cartList = [...cartList];
-    wx.setTabBarBadge({
-      index: 1,
-      text: `${cartList.length}`
-    });
-  },
-  test() {
-    console.log(111)
   }
 });
